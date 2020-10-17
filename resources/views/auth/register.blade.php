279545fdb3ed05ce981fabf6cab06097a17e9694
Ubuntu <ubuntu@ip-172-31-35-9.us-east-2.compute.internal>
2020-10-17 06:36:09 +0000

    Fix register`

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nume) }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Nume') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -40,7 +40,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Parola) }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Parola') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
